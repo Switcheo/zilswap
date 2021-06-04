9f729aa0b4037bbe93d997ae5564a2d2682729b6
Ivan Poon <ravenxce@gmail.com>
2021-06-04 16:32:33 +0800

    Fix max individual cap logic

--- scripts/deploy-zilo-testnet.js
+++ scripts/deploy-zilo-testnet.js
@@ -8,16 +8,16 @@ const deploy = async () => {
   const [contract, state] = await deployZILO(owner.key, {
     zwapAddress:       '0xb2f66571a0c7bd9001fb6ca67b01ac749250504c',
     tokenAddress:      '0x7b8a86afbde45b4e081986217e02654b551407a7',
-    tokenAmount:                 '1000000000000', // STREAM 10k (8 decimals)
-    targetZilAmount:       '2080000000000000000', // ZIL 2m (~$240k)
-    targetZwapAmount:          '428000000000000', // ZWAP 428 (~$128k)
-    minimumZilAmount:       '100000000000000000', // ZIL 100k (~$11k)
-    liquidityZilAmount:     '520000000000000000', // ZIL 520k (~$60k)
-    liquidityTokenAmount:         '180000000000', // STREAM 1.8k (3m ZIL === 10k STREAM, so 520k ZIL ~= 1.7333 STREAM)
+    tokenAmount:                '24000000000000', // STREAM 240k (8 decimals)
+    targetZilAmount:       '1880000000000000000', // ZIL 188k (~$24k @ $0.1282)
+    targetZwapAmount:           '16000000000000', // ZWAP 16 (~$5040 @ $316.32)
+    minimumZilAmount:       '188000000000000000', // ZIL 18.8k (~10%)
+    liquidityZilAmount:     '470000000000000000', // ZIL 47k (~$6k)
+    liquidityTokenAmount:        '6000000000000', // STREAM 60k (188k ZIL === 240k STREAM, so 47k ZIL === 60k STREAM)
     receiverAddress:               owner.address,
     liquidityAddress:   '0x22ed1259dfc29843a481f3801a95429761095366',
     startBlock:           (bNum + 15).toString(),
-    endBlock:        (bNum + (24*60)).toString(),
+    endBlock:        (bNum + (24*60*5)).toString(),
   })
 
   console.log('Deployed contract:')
