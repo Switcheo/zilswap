fb768485d850d61c263d98848c0946eed4fdd39a
junw3i <chanjunwei@gmail.com>
2022-03-10 16:15:25 +0800

    update scripts

--- scripts/deploy-bear-v2.js
+++ scripts/deploy-bear-v2.js
@@ -12,53 +12,61 @@ const deploy = async () => {
 
   const owner = getDefaultAccount()
 
-  console.log("mock bear v2")
-  const [mockNFT, mockState] = await deployBearV2(owner.key, {
-      name: "Mock OG",
-      symbol: "MockBurn",
+  console.log("deploying mock nft")
+  const [mockNFT, mockState] = await deployNonFungibleToken(owner.key, {
     })
+  console.log(mockNFT.address, "mockNFT");
 
-//   console.log("deploying bear v2")
-//   const [nftContract, state] = await deployBearV2(owner.key, {
-//       name: "Bear V2",
-//       symbol: "TBMV2",
-//     })
+  console.log("deploying bear v2")
+  const [nftContract, state] = await deployBearV2(owner.key, {
+      name: "Bear V2",
+      symbol: "TBMV2",
+    })
+  console.log(nftContract.address, "nftContract");
     
-//   console.log("deploying giveaway")
-//   const gmCode = await fs.readFileSync('./src/tbm-v2/GiveawayMinterV2.scilla')
-//   const [gmContract, gmState] = await deployContract(owner.key, gmCode.toString("utf8"), [
-//       {
-//         vname: '_scilla_version',
-//         type: 'Uint32',
-//         value: '0',
-//       },
-//       {
-//         vname: 'contract_owner',
-//         type: 'ByStr20',
-//         value: owner.address,
-//       },
-//       {
-//         vname: 'nft_address',
-//         type: 'ByStr20',
-//         value: nftContract.address,
-//       },
-//       {
-//         vname: 'max_supply',
-//         type: 'Uint128',
-//         value: "200",
-//       },
-//     ])
+  console.log("deploying BurnTBMMinter")
+  const burnCode = await fs.readFileSync('./src/tbm-v2/BurnTBMMinter.scilla')
+  const [burnContract, burnState] = await deployContract(owner.key, burnCode.toString("utf8"), [
+    {
+      vname: '_scilla_version',
+      type: 'Uint32',
+      value: '0',
+    },
+    {
+      vname: 'contract_owner',
+      type: 'ByStr20',
+      value: owner.address,
+    },
+    {
+      vname: 'nft_address',
+      type: 'ByStr20',
+      // value: "0xBA9d440229eE4779DeF4EC09AecceE0626754805",
+      value: nftContract.address,
+    },
+    {
+        vname: 'max_supply',
+        type: 'Uint128',
+        value: "100",
+    },
+    {
+      vname: 'tbm_address',
+      type: 'ByStr20',
+      // value: "0x79D05Fcc4f97EEdd905AFE6C59f0bCd0D4C4BA09",
+      value: mockNFT.address,
+    },
+  ])
+  console.log(burnContract.address, "burnContract");
 
  
 
-  console.log(mockNFT.address, "mockNFT");
+
 //   console.log(gmContract.address, "Giveaway");
 
-  const txn1 = await callContract(owner.key, mockNFT, 'AddMinter', [{
-        vname: 'minter',
-        type: 'ByStr20',
-        value: owner.address.toLowerCase(),
-    }], 0, false, false)
+//   const txn1 = await callContract(owner.key, mockNFT, 'AddMinter', [{
+//         vname: 'minter',
+//         type: 'ByStr20',
+//         value: owner.address.toLowerCase(),
+//     }], 0, false, false)
 }
 
 deploy().then(() => console.log('Done.'))
--- scripts/mint-bear-v2.js
+++ scripts/mint-bear-v2.js
@@ -5,20 +5,36 @@ const { zilliqa } = require('./zilliqa')
 const { fromBech32Address } = require("@zilliqa-js/crypto")
 
 // const CONTRACT_HASH = '0x535416E04080219018717DF62bBE7f6a25e4080E' // giveaway
-const CONTRACT_HASH = '0x79D05Fcc4f97EEdd905AFE6C59f0bCd0D4C4BA09' // nft
+const MOCK_CONTRACT_HASH = '0x5806733973E0Db7f2Ae0d1c9baF54855E85A2c8D' // nft
+const V2_CONTRACT_HASH = '0x42a84501ecf808F2659053f5eD61cF5EA71dB2A3' // nft
+const BURN_CONTRACT_HASH = '0xc7F0A2B82B239f4d5F74301554810f695f012D7a' // nft
 
 // const userAddress = fromBech32Address("zil1r7szwmta4dlkka7qt2ledcmep56474gej3psj6") // steven
 // const userAddress = fromBech32Address("zil1nq8c5zp78lgtf6axvun6pd36ggjx48vzprpy4r") // andrew
 const userAddress = fromBech32Address("zil1gtk045960q0p5akectarewvt4eec3ws73mkn26") // jereld
 // const userAddress = fromBech32Address("zil1r7szwmta4dlkka7qt2ledcmep56474gej3psj6")
 
-const run = async () => {
-    const contract = zilliqa.contracts.at(CONTRACT_HASH)
+const mintMock = async () => {
+    const contract = zilliqa.contracts.at(MOCK_CONTRACT_HASH)
     zilliqa.toBech32Address
     const owner = getDefaultAccount()
     console.log(owner)
 
-    // add minter
+    // configure minter
+    await callContract(
+        owner.key, contract,
+        'ConfigureMinter',
+        [
+            {
+                vname: 'minter',
+                type: 'ByStr20',
+                value: owner.address,
+            }
+        ],
+        0, false, false
+      )
+
+    // add mint
     await callContract(
         owner.key, contract,
         'Mint',
@@ -26,7 +42,7 @@ const run = async () => {
             {
                 vname: 'to',
                 type: 'ByStr20',
-                value: userAddress,
+                value: owner.address,
             },
             {
                 vname: 'token_uri',
@@ -38,4 +54,61 @@ const run = async () => {
       )
 }
 
-run().then(() => console.log('Done.'))
+const burnAndMint = async () => {
+    const mockContract = zilliqa.contracts.at(MOCK_CONTRACT_HASH)
+    const v2Contract = zilliqa.contracts.at(V2_CONTRACT_HASH)
+    const burnContract = zilliqa.contracts.at(BURN_CONTRACT_HASH)
+    zilliqa.toBech32Address
+    const owner = getDefaultAccount()
+    console.log(owner)
+
+    // // add minter
+    // await callContract(
+    //     owner.key, v2Contract,
+    //     'AddMinter',
+    //     [
+    //         {
+    //             vname: 'minter',
+    //             type: 'ByStr20',
+    //             value: BURN_CONTRACT_HASH,
+    //         },
+    //     ],
+    //     0, false, false
+    //   )
+
+    // allow BurnTBM contract to your burn mock nft
+    await callContract(
+        owner.key, mockContract,
+        'SetApprovalForAll',
+        [
+            {
+                vname: 'to',
+                type: 'ByStr20',
+                value: BURN_CONTRACT_HASH.toLowerCase(),
+            },
+        ],
+        0, false, false
+      )
+
+    
+    await callContract(
+        owner.key, burnContract,
+        'BurnAndMint',
+        [
+            {
+                vname: 'to',
+                type: 'ByStr20',
+                value: owner.address,
+            },
+            {
+                vname: 'token_ids',
+                type: 'List Uint256',
+                value: ["1"],
+            },
+        ],
+        0, false, false
+      )
+}
+
+// mintMock().then(() => console.log('Done.'))
+burnAndMint().then(() => console.log('Done.'))
