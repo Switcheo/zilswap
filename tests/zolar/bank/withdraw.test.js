94436ca0d286223983d5b4210b6b354061c31ff4
cheewengg <cheewengg@gmail.com>
2022-07-02 17:48:54 +0800

    refactor test for guildBank withdraw

--- tests/zolar/bank/withdraw.test.js
+++ tests/zolar/bank/withdraw.test.js
@@ -1,47 +1,67 @@
 const { getAddressFromPrivateKey } = require("@zilliqa-js/zilliqa")
 const { default: BigNumber } = require("bignumber.js");
-const { ZERO_ADDRESS, ONE_HUNY, getPrivateKey, deployHuny, deployBankAuthority, deployGuildBank } = require("../../../scripts/zolar/bank/deploy");
-const {callContract} = require('../../../scripts/call')
+const { getPrivateKey, initialEpochNumber, deployHuny, deployZilswap, deployHive, deployBankAuthority, deployGuildBank } = require("../../../scripts/zolar/bank/deploy");
+const {callContract} = require("../../../scripts/call")
 const { getBalanceFromStates, generateErrorMsg } = require("./helper")
 
-let privateKey, address, hunyContract, authorityContract, bankContract, hunyAddress, bankAddress
+let privateKey, address, zilswapAddress, hiveAddress, hunyAddress, authorityAddress, bankAddress, hunyContract, authorityContract, bankContract
 
 beforeAll(async () => {
   privateKey = getPrivateKey();
   address = getAddressFromPrivateKey(privateKey).toLowerCase();
+  
+  memberPrivateKey = getPrivateKey("PRIVATE_KEY_MEMBER")
+  memberAddress = getAddressFromPrivateKey(memberPrivateKey).toLowerCase();
+  
   hunyContract = await deployHuny()
-  authorityContract = await deployBankAuthority({ hiveAddress: ZERO_ADDRESS, hunyAddress: hunyContract.address });
-  bankContract = await deployGuildBank({ authorityAddress: authorityContract.address })
   hunyAddress = hunyContract.address.toLowerCase()
+
+  const zilswapContract = await deployZilswap();
+  zilswapAddress = zilswapContract.address;
+
+  const hiveContract = await deployHive({ hunyAddress, zilswapAddress });
+  hiveAddress = hiveContract.address.toLowerCase();
+  
+  authorityContract = await deployBankAuthority({ initialEpochNumber, hiveAddress, hunyAddress })
+  authorityAddress = authorityContract.address.toLowerCase()
+
+  bankContract = await deployGuildBank({ initialMembers: [], initialEpochNumber, authorityAddress })
   bankAddress = bankContract.address.toLowerCase()
 
-  await callContract(privateKey, hunyContract, "AddMinter", [{
+  const txAddMinter = await callContract(privateKey, hunyContract, "AddMinter", [{
     vname: 'minter',
     type: 'ByStr20',
     value: address,
   }], 0, false, false);
 
-  await callContract(privateKey, hunyContract, "Mint", [{
+  const txMintMember = await callContract(privateKey, hunyContract, "Mint", [{
     vname: 'recipient',
     type: 'ByStr20',
-    value: address,
+    value: memberAddress,
   }, {
     vname: 'amount',
     type: 'Uint128',
-    value: ONE_HUNY.toString(10),
+    value: new BigNumber(1).shiftedBy(12 + 3),
   }], 0, false, false)
 
-  await callContract(privateKey, hunyContract, "IncreaseAllowance", [{
+  // allow member to transfer token to bank (spender)
+  const txAllowanceMember = await callContract(memberPrivateKey, hunyContract, "IncreaseAllowance", [{
     vname: 'spender',
     type: 'ByStr20',
-    value: bankContract.address.toLowerCase(),
+    value: bankAddress,
   }, {
     vname: 'amount',
     type: 'Uint128',
     value: new BigNumber(2).pow(64).minus(1).toString(),
   }], 0, false, false)
 
-  await callContract(privateKey, bankContract, "JoinAndPayJoiningFee", [], 0, false, false)
+  const txApplyMembership = await callContract(memberPrivateKey, bankContract, "ApplyForMembership", [], 0, false, false)
+
+  const txApproveMember = await callContract(privateKey, bankContract, "ApproveAndReceiveJoiningFee", [{
+    vname: "member",
+    type: "ByStr20",
+    value: memberAddress,
+  }], 0, false, false)
 })
 
 test('captain initiate withdrawal tx with sufficient huny in bank', async () => {
@@ -53,16 +73,13 @@ test('captain initiate withdrawal tx with sufficient huny in bank', async () => 
     value: {
       constructor: `${bankAddress}.WithdrawTxParams`,
       argtypes: [],
-      arguments: [address, hunyAddress, ONE_HUNY.toString(10)]
+      arguments: [address, hunyAddress, new BigNumber(0.5).shiftedBy(12).toString(10)]
     },
   }, {
     vname: "message",
     type: "String",
     value: "New withdraw request",
   }], 0, false, false)
-
-  expect(txInitiateWithdrawTx.status).toEqual(2)
-  expect(txInitiateWithdrawTx.receipt.success).toEqual(true)
 
   const hunyContractStateAfterTx = await hunyContract.getState()
   const [bankBalanceBeforeTx, bankBalanceAfterTx] = getBalanceFromStates(bankAddress, hunyContractStateBeforeTx, hunyContractStateAfterTx)
@@ -71,18 +88,20 @@ test('captain initiate withdrawal tx with sufficient huny in bank', async () => 
   const bankWithdrawn = bankBalanceBeforeTx - bankBalanceAfterTx
   const captainReceived = captainBalanceAfterTx - captainBalanceBeforeTx
 
-  expect(bankWithdrawn.toString()).toEqual(ONE_HUNY.toString(10))
-  expect(captainReceived.toString()).toEqual(ONE_HUNY.toString(10))
+  expect(bankWithdrawn.toString()).toEqual(new BigNumber(0.5).shiftedBy(12).toString(10))
+  expect(captainReceived.toString()).toEqual(new BigNumber(0.5).shiftedBy(12).toString(10))
 })
 
 test('captain initiate withdrawal tx with insufficient huny in bank', async () => {
+  const hunyContractStateBeforeTx = await hunyContract.getState()
+
   const txInitiateWithdrawTx = await callContract(privateKey, bankContract, "InitiateTx", [{
     vname: "tx_params",
     type: `${bankAddress}.TxParams`,
     value: {
       constructor: `${bankAddress}.WithdrawTxParams`,
       argtypes: [],
-      arguments: [address, hunyAddress, ONE_HUNY.toString(10)]
+      arguments: [address, hunyAddress, new BigNumber(0.5).shiftedBy(12).toString(10)]
     },
   }, {
     vname: "message",
@@ -90,7 +109,15 @@ test('captain initiate withdrawal tx with insufficient huny in bank', async () =
     value: "New withdraw request",
   }], 0, false, false)
 
+  const hunyContractStateAfterTx = await hunyContract.getState()
+
   expect(txInitiateWithdrawTx.status).toEqual(3)
   expect(txInitiateWithdrawTx.receipt.exceptions[0].message).toEqual(generateErrorMsg(7)) // CodeInvalidTxInsufficientBalance
   expect(txInitiateWithdrawTx.receipt.success).toEqual(false)
+
+  // tx rollback; check NO huny deduction from bank; NO huny increment for captain
+  const [bankBalanceBeforeTx, bankBalanceAfterTx] = getBalanceFromStates(bankAddress, hunyContractStateBeforeTx, hunyContractStateAfterTx)
+  const [captainBalanceBeforeTx, captainBalanceAfterTx] = getBalanceFromStates(address, hunyContractStateBeforeTx, hunyContractStateAfterTx)
+  expect(bankBalanceBeforeTx).toEqual(bankBalanceAfterTx)
+  expect(captainBalanceBeforeTx).toEqual(captainBalanceAfterTx)
 })
