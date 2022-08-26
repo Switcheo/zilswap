3bbb596a7581bfd15a0b0b2269ee9133052b3034
Steven Khong <khongwh@gmail.com>
2022-08-26 10:05:28 +0800

    Update professions deployment script

--- scripts/zolar/mission-2/deploy-professions.js
+++ scripts/zolar/mission-2/deploy-professions.js
@@ -9,21 +9,9 @@ const { deployHunyToken, deployMetazoa, deployProfessions, deployEmporium, deplo
   const address = getAddressFromPrivateKey(privateKey).toLowerCase();
 
   const metazoaAddress = process.env.METAZOA_CONTRACT_HASH;
-  const metazoaContract = zilliqa.contracts.at(metazoaAddress);
 
   const professionsContract = await deployProfessions({ metazoaAddress });
   const professionsAddress = professionsContract.address.toLowerCase();
-
-  // transfer metazoa ownership to professions proxy contract
-  const txTransferOwnership = await callContract(privateKey, metazoaContract, "SetContractOwnershipRecipient", [
-    param('to', 'ByStr20', professionsAddress),
-  ], 0, false, false);
-  console.log("transfer metazoa ownership to proxy", txTransferOwnership.id);
-
-  const txAcceptOwnership = await callContract(privateKey, professionsContract, "AcceptContractOwnership", [
-    param('new_metazoa_address', 'ByStr20', metazoaAddress),
-  ], 0, false, false);
-  console.log("accept metazoa ownership for proxy", txAcceptOwnership.id);
 
   console.log(`\n\n======================`)
   console.log(`\n  Contracts`)
