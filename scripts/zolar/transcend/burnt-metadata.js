a58aeaf79469656b79ad5d26a1f2effb6c69e312
yan-soon <soonxiangyan@gmail.com>
2022-10-18 00:10:28 +0800

    remove funding deck scripts

--- scripts/zolar/transcend/burnt-metadata.js
+++ scripts/zolar/transcend/burnt-metadata.js
@@ -1,5 +1,4 @@
 const fetch = require('node-fetch')
-const BigNumber = require('bignumber.js')
 
 async function getMetadata(tokenUri) {
     const response = await fetch(tokenUri)
@@ -14,58 +13,30 @@ async function getMetadata(tokenUri) {
     }
 }
 
-// 1705000 block height at start of 2022
-// tbm 0xd793f378a925b9f0d3c4b6ee544d31c707899386 zil167flx79fykulp57ykmh9gnf3curcnyux6dcj5e
-// metazoa 0xf79a456a5afd412d3890e2232f6205f664be8957 zil177dy26j6l4qj6wysug3j7cs97ejtaz2h06vfwa
-
 ;
 (async () => {
-    const transcendData = require('./ark-v2-data.json')
+    const transcendData = require('./transcend-data.json')
 
-    const feesMap = {
-        '2021': {},
-        '2022': {},
-    }
-    const proceedsMap = {
-        '2021': {},
-        '2022': {},
-    }
-    const zeroAddress = 'zil000000000000000000000000000000000000000'
-    
-    let count1 = 0
-    let count2 = 0
-    for (const data of transcendData) {
-        const height = data.blockHeight
-        const events = data.events
-        const tradeEvent = events.find(event => event.name === 'ExecuteTradeSuccess')
-        if (tradeEvent) {
-            const { token, proceeds, fees } = tradeEvent.params
-            const tokenAddress = token[0].params[0]
-            if (tokenAddress === 'zil177dy26j6l4qj6wysug3j7cs97ejtaz2h06vfwa') {
-                const year = parseInt(height) < 1705000 ? '2021' : '2022'
-                parseInt(height) < 1705000 ? count1++ : count2++
-                const proceedAmt = proceeds[0].params[0]
-                const feeAmt = fees[0].params[0]
-                const tokenType = proceeds[1].name.split('.')[1]
-                if (tokenType === "Zil") {
-                    if (!feesMap[year][zeroAddress]) feesMap[year][zeroAddress] = new BigNumber(0)
-                    if (!proceedsMap[year][zeroAddress]) proceedsMap[year][zeroAddress] = new BigNumber(0)
+    let data = []
+    const transcendTxs = transcendData.filter((event) => JSON.parse(event.data)._tag === "Transcend")
+    const transcendEvents = transcendTxs.map(tx => tx.events)
 
-                    feesMap[year][zeroAddress] = feesMap[year][zeroAddress].plus(new BigNumber(feeAmt))
-                    proceedsMap[year][zeroAddress] = proceedsMap[year][zeroAddress].plus(new BigNumber(proceedAmt))
-                } else {
-                    const paymentToken = proceeds[1].params[0]
-                    if (!feesMap[year][paymentToken]) feesMap[year][paymentToken] = new BigNumber(0)
-                    if (!proceedsMap[year][paymentToken]) proceedsMap[year][paymentToken] = new BigNumber(0)
-
-                    feesMap[year][paymentToken] = feesMap[year][paymentToken].plus(new BigNumber(feeAmt))
-                    proceedsMap[year][paymentToken] = proceedsMap[year][paymentToken].plus(new BigNumber(proceedAmt))
-                }
+    for (const events of transcendEvents) {
+        let index = 0
+        while (index < events.length) {
+            if (events[index].name !== 'Mint' || events[index + 1].name !== 'BurnSuccess')
+            throw new Error('invalid transaction order')
+            const mintZolarId = events[index].params.token_id
+            const burntTBMId = events[index + 1].params.token
+            const burntMetadata = await getMetadata('https://api.thebear.market/metadata/' + burntTBMId)
+            const entry = {
+                mintZolarId,
+                burntTBMId,
+                burntMetadata: burntMetadata.attributes[0]
             }
+            data.push(entry)
+            index+=2
         }
     }
-    console.log("Proceeds:", proceedsMap[2021])
-    console.log("Fees: ", feesMap[2021])
-    console.log(count1)
-    console.log(count2)
+    console.log(data)
 })().catch(console.error).finally(() => process.exit(0))
