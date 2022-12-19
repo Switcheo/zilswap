827ad12eaac4bb3335450ab55dd0ddbb48c61883
kaijun123 <kaijun123@gmail.com>
2022-12-19 17:57:17 +0800

    Enabled IsValidPoolContract check on router

--- tests/zilswap-v2/helper.js
+++ tests/zilswap-v2/helper.js
@@ -1,8 +1,12 @@
 const fs = require("fs");
 const Crypto = require("crypto");
+const { compress } = require("../../scripts/compile");
 
 const getContractCodeHash = (file) => {
-  return "0x" + Crypto.createHash("sha256").update(fs.readFileSync(file)).digest("hex");
+  const code = fs.readFileSync(file).toString()
+  const compressedFile = compress(code)
+  const buffer = Buffer.from(compressedFile)
+  return "0x" + Crypto.createHash("sha256").update(buffer).digest("hex");
 };
 
 module.exports = {
